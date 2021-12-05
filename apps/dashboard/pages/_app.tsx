f72c900042c0d5c39feecb9ca219b10932288d76
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2021-12-05 22:40:56 +0100

    feat: the road so far

---
+++ apps/dashboard/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to dashboard!</title>
+      </Head>
+      <div className="app">
+        <header className="flex">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
+          <h1>Welcome to dashboard!</h1>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
